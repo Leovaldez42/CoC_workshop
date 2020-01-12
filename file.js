f49399305ccdcc2065b7c1718469d1d204f6106b
Leovaldez42 <gaurav4.037@gmail.com>
2020-01-12 19:34:09 +0530

    project uploaded successfully

---
+++ file.js
@@ -0,0 +1,19 @@
+var audio1 = document.getElementById("myAudio1")
+var audio2 = document.getElementById("myAudio2")
+var audio3 = document.getElementById("myAudio3")
+
+function playAudio1() {
+    audio1.play();
+}
+
+function playAudio2() {
+    audio2.play();
+}
+
+function playAudio3() {
+    audio3.play()
+}
+
+function display() {
+    alert('Finally khatam hua ')
+}
